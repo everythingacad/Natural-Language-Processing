3865373e54438e030c41819319bcbefa8063dc8e
samanyu6 <samanyu6@yahoo.co.in>
2020-07-08 19:39:16 +0530

    first commit

--- src/components/Shortcut.vue
+++ src/components/Shortcut.vue
@@ -10,13 +10,13 @@
 </template>
 
 <script>
-import { PlayIcon, SlidersIcon, EyeIcon } from 'vue-feather-icons'
+import { BookOpenIcon, TerminalIcon, TrendingUpIcon } from 'vue-feather-icons'
 
 export default {
   components: {
-    PlayIcon,
-    SlidersIcon,
-    EyeIcon
+    BookOpenIcon, 
+    TerminalIcon, 
+    TrendingUpIcon
   },
   props: {
     link: {
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,12 +1,29 @@
 <template>
   <Layout :sidebar="false">
     <div class="content">
-      <h1>{{ $static.metadata.siteName }} - {{ this.description }}</h1>
+      <!-- <h1>{{ $static.metadata.siteName }} - {{ this.description }}</h1> -->
+      <h1>{{ this.description }}</h1>
+      <div class="desc">
+        <div class="content-desc">
+          <h2>What is this?</h2>
+          <h3>{{this.what}}</h3>
+        </div>
+
+         <div class="content-desc">
+          <h2>Where do we find this?</h2>
+          <h3>{{this.where}}</h3>
+        </div>
+
+         <div class="content-desc">
+          <h2>Why do we need this?</h2>
+          <h3>{{this.why}}</h3>
+        </div>
+      </div>
       <nav>
         <!-- To use other icons here, you need to import them in the Shortcut component -->
-        <Shortcut link="/getting-started" text="Introduction" icon="play-icon" />
-        <Shortcut link="/theme-configuration" text="Configuration" icon="sliders-icon" />
-        <Shortcut link="/theme-configuration#changing-colors" text="Change colors" icon="eye-icon" />
+        <Shortcut link="/getting-started" text="Rookie" icon="book-open-icon" />
+        <Shortcut link="/theme-configuration" text="Intermediate" icon="terminal-icon" />
+        <Shortcut link="/theme-configuration#changing-colors" text="Production" icon="trending-up-icon" />
       </nav>
       <GitLink class="git" size="large" />
     </div>
@@ -32,7 +49,11 @@ export default {
   },
   data() {
     return {
-      description: 'The ultimate static generated documentation theme for the JAM-stack'
+      description: `Natural Language Processing`,
+      what: `The science of teaching your computer to speak and understand your spoken language. SkyNet heavily depends on this.`,
+      where: `Asking Alexa, Siri and Google to do something for you! That's just one of the many million applications out there.`,
+      why: ` Imagine the possibilites of an intelligent talking computer. From analysing millions of documents to an AI that can write plays and lyrics!`,
+      // how: ,
     }
   },
   metaInfo() {
@@ -52,19 +73,44 @@ export default {
   flex-direction: column;
 }
 
+
 h1 {
   text-align: center;
   max-width: 600px;
   margin: 1.5em auto 1.5em;
+  font-size: 4em;
 
   @include respond-above(md) {
     max-width: 1000px;
   }
 }
 
+.desc{
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+
+.content-desc {
+  display: flex;
+  flex-direction: column;
+  // max-width: 35%;
+  word-wrap: break;
+  flex-wrap: wrap;
+  margin: auto;
+}
+
 h2 {
-  font-size: 1.4rem;
-  margin: 0;
+  font-size: 2rem;
+  margin: auto auto 0.5em auto;
+  text-align: center;
+}
+
+h3 {
+  font-size: 1.5em;
+  text-align: center;
+  margin: auto auto 3em auto;
+  max-width: 25ch;
 }
 
 nav {
